a5fbf2105cf32cef0e267882a7fe5315be32e036
cosmos hagan <cosmoshagan@cosmoss-MacBook-Pro.local>
2023-02-23 09:39:23 +0000

    Update index.js and style.css

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 import './style.css';
 
 const tasks = [{
-        description: 'Coding milestone projects',
-        completed: false,
-        index: 1,
-    },
-    {
-        description: 'Learning JS ES6',
-        completed: false,
-        index: 2,
-    },
-    {
-        description: 'Attending morning session meeting',
-        completed: false,
-        index: 3,
-    },
-    {
-        description: 'Going for lunch break',
-        completed: false,
-        index: 4,
-    },
+  description: 'Coding milestone projects',
+  completed: false,
+  index: 1,
+},
+{
+  description: 'Learning JS ES6',
+  completed: false,
+  index: 2,
+},
+{
+  description: 'Attending morning session meeting',
+  completed: false,
+  index: 3,
+},
+{
+  description: 'Going for lunch break',
+  completed: false,
+  index: 4,
+},
 ];
 
 const itemList = document.getElementById('addItem');
 
 function item() {
-    itemList.innerHTML = tasks.map((task) => `<li class="items">
+  itemList.innerHTML = tasks.map((task) => `<li class="items">
     <label for="items"><input class="check" type="checkbox">${task.description}</label>
 </li>`).join();
 }
 window.onload = () => {
-    item();
+  item();
 };
